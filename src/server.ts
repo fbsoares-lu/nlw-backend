ab1b1836892a845fb77c005c4fcfc9144a2fe9e3
Lucas Soares <lsoareshfb@gmail.com>
2020-10-18 13:40:15 -0300

    Criado os controllers

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,12 @@ import express from 'express';
 
 import './database/connection';
 
+import routes from './routes';
+
 const app = express();
 
-app.get('/orphanges', (request, response) => {
-    
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
     'Server is Started'
